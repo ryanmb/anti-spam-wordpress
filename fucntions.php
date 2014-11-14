ad788154aad49bdfdd043e415e13ef76e4d1fe8e
Ryan Morgan <ryan@ryanmorgan.co>
2014-11-14 02:40:36 +0000

    Hide email from spam bots

---
+++ fucntions.php
@@ -0,0 +1,9 @@
+<?php
+// ---------- HIDE EMAIL ADDRESS ----------
+function wpcodex_hide_email_shortcode( $atts , $content = null ) {
+	if ( ! is_email( $content ) ) {
+		return;
+	}
+	return '<a href="mailto:' . antispambot( $content ) . '">' . antispambot( $content ) . '</a>';
+}
+add_shortcode( 'email', 'wpcodex_hide_email_shortcode' );
